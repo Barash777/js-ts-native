29a3bd1b538585b138e68a0a85e86f824e2821a5
Barash777 <barash.vertihvost@gmail.com>
2022-05-28 19:43:13 +0300

    lesson 01 (tests)

---
+++ src/01/01.test.ts
@@ -0,0 +1,60 @@
+import {mult, splitIntoWords, sum} from './01';
+
+// data
+let a: number
+let b: number
+let c: number
+
+beforeEach(() => {
+    a = 1;
+    b = 2;
+    c = 3;
+})
+
+
+test('sum check', () => {
+    // action
+    const resultAB = sum(a, b)
+    const resultAC = sum(a, c)
+    const resultBC = sum(b, c)
+
+    // expected result
+    expect(resultAB).toBe(3)
+    expect(resultAC).toBe(5)
+    expect(resultBC).toBe(6)
+})
+
+test('multiply check', () => {
+    // action
+    const resultAB = mult(a, b)
+    const resultAC = mult(a, c)
+    const resultBC = mult(b, c)
+
+    // expected result
+    expect(resultAB).toBe(3)
+    expect(resultAC).toBe(5)
+    expect(resultBC).toBe(15)
+})
+
+test('split into the words check', () => {
+
+    // data
+    const sent1 = 'hello world'
+    const sent2 = 'my name is Eric'
+
+    // action
+    const resultA = splitIntoWords(sent1)
+    const resultB = splitIntoWords(sent2)
+
+    // expected result
+    expect(resultA.length).toBe(2)
+    expect(resultA[0]).toBe('hello')
+    expect(resultA[1]).toBe('world')
+
+
+    expect(resultB.length).toBe(4)
+    expect(resultB[0]).toBe('my')
+    expect(resultB[1]).toBe('name')
+    expect(resultB[2]).toBe('is')
+    expect(resultB[3]).toBe('eric')
+})
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {splitIntoWords} from './01/01';
+
+function App() {
+
+    // console.log(splitIntoWords('hello world'))
+
+    return (
+        <div className="App">
+            learn react
+        </div>
+    );
+}
+
+export default App;
